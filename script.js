65132beef39364488b372ec36f20cb09e4d4d183
Darko <dj_mamlyooo@hotmail.com>
2023-11-05 14:05:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const langBtn = document.getElementById("langBtn");
+  let currentLang = "en";
+
+  langBtn.addEventListener("click", () => {
+    if (currentLang === "en") {
+      currentLang = "de";
+      langBtn.style.backgroundImage = "url('./public/english.jpg')";
+      langBtn.textContent = " "; // This will hide the text, making space for the flag image
+    } else {
+      currentLang = "en";
+      langBtn.style.backgroundImage = "url('./public/german.jpg')";
+      langBtn.textContent = " "; // This will hide the text, making space for the flag image
+    }
+
+    const elementsWithData = document.querySelectorAll("[data-en], [data-de]");
+    elementsWithData.forEach((el) => {
+      if (currentLang === "en") {
+        if (el.hasAttribute("data-en"))
+          el.textContent = el.getAttribute("data-en");
+      } else {
+        if (el.hasAttribute("data-de"))
+          el.textContent = el.getAttribute("data-de");
+      }
+    });
+  });
+
+  //to top button
+
+  const toTopButton = document.getElementById("toTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      toTopButton.style.display = "block";
+    } else {
+      toTopButton.style.display = "none";
+    }
+  });
+
+  toTopButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
+
+//navbar
+
+document.addEventListener("DOMContentLoaded", function () {
+  let menuIcon = document.querySelector(".menu-icon");
+  let navbar = document.querySelector(".navbar");
+  let navbarLinks = document.querySelectorAll(".navbar-link");
+
+  // Open the navbar dropdown on hover over the menu icon
+  menuIcon.addEventListener("mouseenter", function () {
+    navbar.classList.add("responsive");
+  });
+
+  // Close the navbar dropdown when the mouse leaves the navbar area
+  navbar.addEventListener("mouseleave", function () {
+    navbar.classList.remove("responsive");
+  });
+
+  // Close the navbar dropdown after a navbar link is clicked
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navbar.classList.remove("responsive");
+    });
+  });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    console.log("Form submitted. Fetching values...");
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log(`Fetched values - Name: ${name}, Email: ${email}, Message: ${message}`);
+
+    fetch('http://localhost:3000/send-email', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            message,
+        }),
+    })
+    .then(response => response.json())  // <-- Add this line to parse the response as JSON
+    .then(data => {
+        console.log(`Server responded with:`, data);
+        if (data.status === 'success') {  // <-- Change this line to check the 'status' property
+            alert('Email sent successfully!');
+        } else {
+            alert('Error sending email. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error during the fetch:', error.message);
+        alert('Error sending email. Please check the console for more details.');
+    });
+});
+
---
+++ server.mjs
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the "public" directory
+app.use(cors());
+app.use(express.static("public"));
+
+// Replace bodyParser with express's built-in methods
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(`request received: ${name}, ${email}, ${message}`);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mail.yahoo.com',
+    port: 465,
+    service:'yahoo',
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: `New message from ${name}`,
+    text: `From: ${email}\nMessage: ${message}`,
+};
+
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Failed to send email:", error);
+      return res.status(500).send("Internal Server Error");
+    }
+    console.log("Email sent:", info.response);
+    res.json({ status: 'success' });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
